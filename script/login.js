82ce23985c21f5eb9e41d6cbf356115f12249067
GabrielSantos29 <142807683+GabrielSantos29@users.noreply.github.com>
2024-01-06 11:32:19 -0300

    03

---
+++ script/login.js
@@ -0,0 +1,7 @@
+var containerimg = document.getElementById('containerimg');
+var containerbtn = document.getElementById('containerbtn');
+var containerform = document.getElementById('containerform');
+
+function login(){
+        containerform.style.display='block'
+}
