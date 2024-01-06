e4a333bdf62d519e22d626a7785798c571a4e5a9
GabrielSantos29 <142807683+GabrielSantos29@users.noreply.github.com>
2024-01-06 12:08:58 -0300

    06

--- script/login.js
+++ script/login.js
@@ -4,7 +4,9 @@ var containerform = document.getElementById('containerform');
 
 function login(){
         if (containerform.style.display=='none'){
-                containerform.style.display='block'
-        } else {containerform.style.display='none'}
+                containerform.style.display='block';
+                containerimg.style.height='30vh';
+        } else {containerform.style.display='none'
+                containerimg.style.height='60vh';}
 
 }
